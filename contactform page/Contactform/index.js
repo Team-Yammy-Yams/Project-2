4c9d558d9a822557e7018b600c1bf0cdf82c3c51
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-11-06 23:31:42 -0600

    Delete index.js

--- contactform page/Contactform/index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("button").addEventListener("click", submitfunction);
-
-function submitFunction() {
-  alert("Thank you for the message! We will get back to you within 1-2 weeks");
-}

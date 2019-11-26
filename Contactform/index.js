9032165b8240b55813f678b7f7d03a97f49451cd
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-11-26 13:33:41 -0600

    deleted this js and move it to its own folder

--- Contactform/index.js
+++ Contactform/index.js
@@ -1,5 +1,1 @@
-document.getElementById("button").addEventListener("click", submitfunction);
 
-function submitFunction() {
-  alert("Thank you for the message! We will get back to you within 1-2 weeks");
-}

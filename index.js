5eaf72001c1c770c2219db504c7b1618d9a2e413
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-11-26 14:27:41 -0600

    updated contact html button id and js field email validation

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-document.getElementById("button").addEventListener("click", submitfunction);
+document.getElementById("button").addEventListener("click", submitFunction);
 
 function submitFunction() {
   alert("Thank you for the message! We will get back to you within 1-2 weeks");
+}
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
 }

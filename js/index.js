d658fcc6fc573075cdbd3e56378384cb4ec57f09
YelyzavetaMyshenyova <46732280+YelyzavetaMyshenyova@users.noreply.github.com>
2019-11-27 22:10:51 -0600

    fix cascade

--- js/index.js
+++ js/index.js
@@ -1,23 +1,21 @@
 document.getElementById("button").addEventListener("click", submitFunction);
 
 function submitFunction() {
-	if(ValidateEmail(document.getElementById("email").value)== true)
-		alert("Thank you for the message! We will get back to you within 1-2 weeks");
-	else
-		alert("You have entered an invalid email address!");
+  if(ValidateEmail(document.getElementById("email").value)== true)
+  alert("Thank you for the message! We will get back to you within 1-2 weeks");
+  else
+  alert("You have entered an invalid email address!");
 }
-function ValidateEmail(inputText)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(inputText.value.match(mailformat))
-{
-document.form1.text1.focus();
-return true;
-}
+function ValidateEmail(inputText){
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(inputText.value.match(mailformat)){
+    document.form1.text1.focus();
+    return true;
+  }
 else
-{
-alert("You have entered an invalid email address!");
-document.form1.text1.focus();
-return false;
-}
+  {
+  alert("You have entered an invalid email address!");
+  document.form1.text1.focus();
+  return false;
+  }
 }
